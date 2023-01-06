5e98485cbe7fa17c0d58f8f405bf8473c8d5088d
EvieButland <evie.butland@hectare.farm>
2023-01-06 07:45:39 +0000

    practice

---
+++ test.py
@@ -0,0 +1,15 @@
+test = 1
+
+astring = "Hello world!"
+print(astring[3:7])
+# This prints out a slice (lo w) of the
+# characters starting at an index of 3 and ending at 7
+
+print(astring[3:7:2])
+# [start:stop:step]
+
+
+print(astring.count("l"))
+# This counts the number of l's in the string
+print(len(astring))
+# Print the lenght of the string
